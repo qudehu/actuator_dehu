f42b83054df530139ebfbfffe56567a5269dde35
qudehu <qudehu@163.com>
2023-12-12 21:23:51 +0800

    actuator的初始化项目搭建

---
+++ actuator_hu/src/main/java/com/dehu/actuator_hu/ActuatorHuApplication.java
@@ -0,0 +1,13 @@
+package com.dehu.actuator_hu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorHuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorHuApplication.class, args);
+    }
+
+}
---
+++ actuator_hu/src/test/java/com/dehu/actuator_hu/ActuatorHuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dehu.actuator_hu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorHuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
