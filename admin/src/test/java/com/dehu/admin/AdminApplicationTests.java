d555db6abef9bd61199677f4adf17cf5bfec263b
qudehu <qudehu@163.com>
2023-12-13 20:18:49 +0800

    项目搭建及SpringBootAdmin依赖添加

---
+++ admin/src/test/java/com/dehu/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dehu.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
