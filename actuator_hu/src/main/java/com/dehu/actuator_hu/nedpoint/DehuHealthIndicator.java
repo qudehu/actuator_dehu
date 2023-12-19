4edd4afe4196284cedd932de97c9ecc57c97574a
qudehu <qudehu@163.com>
2023-12-19 21:02:21 +0800

    server:定制endpoint

---
+++ actuator_hu/src/main/java/com/dehu/actuator_hu/nedpoint/DehuHealthIndicator.java
@@ -0,0 +1,33 @@
+package com.dehu.actuator_hu.nedpoint;
+
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DehuHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+
+        try {
+            boolean ischeck = check();
+
+            if (ischeck) {
+                builder.up().withDetail("total", "1");
+            } else {
+                builder.down();
+            }
+        } catch (Exception ex) {
+            builder.down().withException(ex);
+        }
+    }
+
+    // 自己去连接mongdb 如果连不上说明状态down
+    // 根据自己项目的业务需求定制
+    private boolean check() {
+        // TODO: 验证逻辑
+        int a = 1 / 0;
+        return true;
+    }
+}
