23db0cd6b87a91f930c015d2fc44d2c976125158
qudehu <qudehu@163.com>
2023-12-19 21:38:56 +0800

    server:代码形式定制Metrics

--- actuator_hu/src/main/java/com/dehu/actuator_hu/controller/HelloController.java
+++ actuator_hu/src/main/java/com/dehu/actuator_hu/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.dehu.actuator_hu.controller;
 
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/sayHi")
-    public String sayHi() throws InterruptedException {
+    public String sayHi(String username) throws InterruptedException {
+// 线程长请求
+        //Thread.sleep(5000);
+        // 定制基于Metrics的计数器
+        Metrics.counter("qudehu.counter","username",username).increment();
 
-        Thread.sleep(5000);
+        // 定制基于Metrics的定时器
+        Timer timer = Metrics.timer("qudehu.timer");
 
-        System.out.println("success");
+        timer.record(()->{
+            System.out.println("success");
+        });
 
+        // 定制基于Metrics的仪表  记录单个值  一般集合数量
+        Metrics.gauge("qudehu.gauge",1);
+
+        // 定制基于Metrics的摘要 一般用来记录 百分比数值（缓存命中率)
+        DistributionSummary summary = Metrics.summary("test.summary");
+        summary.record(1.5);
         return "success";
     }
 
