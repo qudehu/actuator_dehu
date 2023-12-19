77b56514831d754eb54d2387de1e2a60ddd2931b
qudehu <qudehu@163.com>
2023-12-19 21:39:18 +0800

    server:代码形式定制Metrics

--- actuator_hu/src/main/java/com/dehu/actuator_hu/controller/HelloController.java
+++ actuator_hu/src/main/java/com/dehu/actuator_hu/controller/HelloController.java
@@ -12,19 +12,19 @@ public class HelloController {
     @RequestMapping("/sayHi")
     public String sayHi(String username) throws InterruptedException {
 // 线程长请求
-        //Thread.sleep(5000);
+        // Thread.sleep(5000);
         // 定制基于Metrics的计数器
-        Metrics.counter("qudehu.counter","username",username).increment();
+        Metrics.counter("qudehu.counter", "username", username).increment();
 
         // 定制基于Metrics的定时器
         Timer timer = Metrics.timer("qudehu.timer");
 
-        timer.record(()->{
+        timer.record(() -> {
             System.out.println("success");
         });
 
         // 定制基于Metrics的仪表  记录单个值  一般集合数量
-        Metrics.gauge("qudehu.gauge",1);
+        Metrics.gauge("qudehu.gauge", 1);
 
         // 定制基于Metrics的摘要 一般用来记录 百分比数值（缓存命中率)
         DistributionSummary summary = Metrics.summary("test.summary");
